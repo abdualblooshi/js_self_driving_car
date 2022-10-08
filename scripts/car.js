919a0883606d65e4b17714f865ddc9ce7eefc000
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-08 22:02:24 +0400

    Created a car class script

---
+++ scripts/car.js
@@ -0,0 +1,36 @@
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.controls = new Controls();
+  }
+
+  update() {
+    if (this.controls.forward) {
+      this.y -= 1;
+    }
+    if (this.controls.left) {
+      this.x -= 1;
+    }
+    if (this.controls.right) {
+      this.x += 1;
+    }
+    if (this.controls.reverse) {
+      this.y += 1;
+    }
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+  }
+}
