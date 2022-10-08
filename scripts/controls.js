0756c6c0d5600fd111815063e962799278febf68
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-08 22:30:21 +0400

    Removed debugging lines for controls.js

--- scripts/controls.js
+++ scripts/controls.js
@@ -27,7 +27,6 @@ class Controls {
           this.reverse = true;
           break;
       }
-      console.table(this);
     };
     document.onkeyup = (event) => {
       switch (event.key) {
@@ -47,7 +46,6 @@ class Controls {
           this.reverse = false;
           break;
       }
-      console.table(this);
     };
   }
 }
