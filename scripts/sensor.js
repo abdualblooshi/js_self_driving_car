dd836890b33ab3195df86da9b677abeb5a9cc3f8
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-09 16:55:13 +0400

    Added some code to enable having rayCount as 1 ray when needed

--- scripts/sensor.js
+++ scripts/sensor.js
@@ -10,20 +10,21 @@
 class Sensor {
   constructor(car) {
     this.car = car;
-    this.rayCount = 3;
-    this.rayLength = 100;
-    this.raySpread = Math.PI / 4; // 45 degrees
+    this.rayCount = 5;
+    this.rayLength = 150;
+    this.raySpread = Math.PI / 2; // 45 degrees
     this.rays = [];
   }
 
   update() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
-      const rayAngle = lerp(
-        this.raySpread / 2,
-        -this.raySpread / 2,
-        i / (this.rayCount - 1)
-      );
+      const rayAngle =
+        lerp(
+          this.raySpread / 2,
+          -this.raySpread / 2,
+          this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+        ) + this.car.angle;
 
       const start = { x: this.car.x, y: this.car.y };
       const end = {
@@ -33,9 +34,11 @@ class Sensor {
       this.rays.push([start, end]);
     }
   }
+
   draw(ctx) {
     for (let i = 0; i < this.rays.length; i++) {
       const ray = this.rays[i];
+      ctx.strokeStyle = "#fff000";
       ctx.beginPath();
       ctx.moveTo(ray[0].x, ray[0].y);
       ctx.lineTo(ray[1].x, ray[1].y);
