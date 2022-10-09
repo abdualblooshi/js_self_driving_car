36ae03c439354351dce0ee048473140d2ce9974a
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-09 16:37:29 +0400

    Created a getIntersection function for the sensor system

--- scripts/utils.js
+++ scripts/utils.js
@@ -11,3 +11,22 @@ function lerp(A, B, t) {
   // 0 and 100 at 50%, you would use lerp(0, 100, 0.5) which would return 50.
   return A + (B - A) * t;
 }
+
+function getIntersection(A, B, C, D) {
+  // Returns the intersection point between two lines AB and CD
+  // The intersection point is the point where the two lines intersect.
+  // If the lines are parallel, then the function returns null.
+  const denominator = (A.x - B.x) * (C.y - D.y) - (A.y - B.y) * (C.x - D.x);
+  if (denominator == 0) {
+    return null;
+  }
+  const x =
+    ((A.x * B.y - A.y * B.x) * (C.x - D.x) -
+      (A.x - B.x) * (C.x * D.y - C.y * D.x)) /
+    denominator;
+  const y =
+    ((A.x * B.y - A.y * B.x) * (C.y - D.y) -
+      (A.y - B.y) * (C.x * D.y - C.y * D.x)) /
+    denominator;
+  return { x, y };
+}
