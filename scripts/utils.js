f071a1e5ad36f1b7fd4f9fe4ccace80229c265d2
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-09 16:25:39 +0400

    Added documentation for utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,13 @@
+/*
+    Author : @gitanimous
+    Utilities
+    - contains a collection of useful functions for use in the simulation.
+*/
+
 function lerp(A, B, t) {
+  // Returns the linear interpolation between A and B at time t
+  // Linear interpolation is a simple way to calculate a value between two values
+  // based on a percentage. For example, if you want to calculate a value between
+  // 0 and 100 at 50%, you would use lerp(0, 100, 0.5) which would return 50.
   return A + (B - A) * t;
 }
