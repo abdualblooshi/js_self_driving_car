670139adc233aa3b7b4cf403ccff0f33a76490c8
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-09 16:37:15 +0400

    Created a new sensor class and added a ray system

--- scripts/sensor.js
+++ scripts/sensor.js
@@ -13,5 +13,33 @@ class Sensor {
     this.rayCount = 3;
     this.rayLength = 100;
     this.raySpread = Math.PI / 4; // 45 degrees
+    this.rays = [];
+  }
+
+  update() {
+    this.rays = [];
+    for (let i = 0; i < this.rayCount; i++) {
+      const rayAngle = lerp(
+        this.raySpread / 2,
+        -this.raySpread / 2,
+        i / (this.rayCount - 1)
+      );
+
+      const start = { x: this.car.x, y: this.car.y };
+      const end = {
+        x: this.car.x - this.rayLength * Math.sin(rayAngle),
+        y: this.car.y - this.rayLength * Math.cos(rayAngle),
+      };
+      this.rays.push([start, end]);
+    }
+  }
+  draw(ctx) {
+    for (let i = 0; i < this.rays.length; i++) {
+      const ray = this.rays[i];
+      ctx.beginPath();
+      ctx.moveTo(ray[0].x, ray[0].y);
+      ctx.lineTo(ray[1].x, ray[1].y);
+      ctx.stroke();
+    }
   }
 }
