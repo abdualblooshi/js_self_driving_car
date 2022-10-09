790b063eb3208baec9213a3d5519fd7170f1902e
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-09 17:24:06 +0400

    Updated the getIntersection function to another algorithm

--- scripts/utils.js
+++ scripts/utils.js
@@ -13,20 +13,28 @@ function lerp(A, B, t) {
 }
 
 function getIntersection(A, B, C, D) {
-  // Returns the intersection point between two lines AB and CD
-  // The intersection point is the point where the two lines intersect.
-  // If the lines are parallel, then the function returns null.
-  const denominator = (A.x - B.x) * (C.y - D.y) - (A.y - B.y) * (C.x - D.x);
-  if (denominator == 0) {
-    return null;
+  // Returns the intersection point between two lines
+  // The lines are defined by the points A,B and C,D
+  // The intersection point is returned as an object with the x and y coordinates
+  // and the offset from point A. The offset is the distance along the line AB
+  // from point A to the intersection point.
+  // If the lines are parallel, the function returns null.
+
+  const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+  const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+  const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+  if (bottom != 0) {
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: lerp(A.x, B.x, t),
+        y: lerp(A.y, B.y, t),
+        offset: t,
+      };
+    }
   }
-  const x =
-    ((A.x * B.y - A.y * B.x) * (C.x - D.x) -
-      (A.x - B.x) * (C.x * D.y - C.y * D.x)) /
-    denominator;
-  const y =
-    ((A.x * B.y - A.y * B.x) * (C.y - D.y) -
-      (A.y - B.y) * (C.x * D.y - C.y * D.x)) /
-    denominator;
-  return { x, y };
+
+  return null;
 }
