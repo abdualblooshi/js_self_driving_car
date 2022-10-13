b9bc37ac4d361a521d4f73d894daab22eb8045fb
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-13 18:38:52 +0400

    Traffic Array the holds all the AI cars

--- scripts/main.js
+++ scripts/main.js
@@ -10,27 +10,46 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "AI")];
 animate();
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  traffic.forEach((car) => {
+    car.update(road.borders);
+  });
   car.update(road.borders);
+
   canvas.height = window.innerHeight;
+
   ctx.save();
   ctx.translate(0, -car.y + canvas.height / 1.5);
+
   road.draw(ctx);
-  car.draw(ctx);
+  traffic.forEach((trafficCar) => {
+    trafficCar.draw(ctx);
+  });
   if (car.damaged) {
     setTimeout(function () {
-      car.x = road.getLaneCenter(1);
-      car.speed = 5;
-      car.angle = 0;
-      car.damaged = false;
       let message = document.getElementById("message");
       message.style.display = "none";
-    }, 1000);
+      reloadOnce();
+    }, 2000);
   }
+  car.draw(ctx);
   ctx.restore();
   requestAnimationFrame(animate);
+}
+
+let reloaded = false;
+
+function reloadGame() {
+  if (!reloaded) {
+    reloaded = true;
+    window.location.reload();
+  }
+}
+
+function reloadOnce() {
+  if (!reloaded) reloadGame();
 }
