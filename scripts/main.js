d727a1c7c86a573825b096c73367f50a88613e92
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-13 23:33:31 +0400

    Changed traffic cars to "DUMMY" and user car to "AI"

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,8 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 4);
-const traffic = [new Car(road.getLaneCenter(2), -100, 30, 50, "AI", 2, 3)];
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 4);
+const traffic = [new Car(road.getLaneCenter(2), -100, 30, 50, "DUMMY", 2, 3)];
 let carLocation = car.y;
 setTimeout(() => {
   setInterval(() => {
@@ -91,7 +91,7 @@ function generateTraffic(carLocation) {
       randomLocation,
       30,
       50,
-      "AI",
+      "DUMMY",
       2,
       randomIntFromInterval(1, 3)
     );
