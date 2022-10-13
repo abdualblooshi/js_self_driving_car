d97c4da939133bb0904e82b28165a60c92131852
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-13 18:38:21 +0400

    Added a new controlType variable to distinguish AI and user cars

--- scripts/car.js
+++ scripts/car.js
@@ -1,5 +1,5 @@
 class Car {
-  constructor(x, y, width, height) {
+  constructor(x, y, width, height, controlType) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -14,7 +14,7 @@ class Car {
     this.isMoving = false;
 
     this.sensor = new Sensor(this);
-    this.controls = new Controls();
+    this.controls = new Controls(controlType);
   }
 
   update(roadBorders) {
@@ -33,6 +33,7 @@ class Car {
       message.style.flexDirection = "column";
       message.style.alignItems = "center";
       message.style.justifyContent = "center";
+      message.style.height = "50px";
       messageText.innerHTML = "<h1>💥 You crashed!</h1>";
     }
 
