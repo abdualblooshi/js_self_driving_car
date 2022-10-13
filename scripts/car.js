8ca1a68feebd86b362c5c32946dc8d0724029d6d
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-13 18:42:41 +0400

    Added the maxSpeed variable to the constructor arguments

--- scripts/car.js
+++ scripts/car.js
@@ -1,5 +1,5 @@
 class Car {
-  constructor(x, y, width, height, controlType) {
+  constructor(x, y, width, height, controlType, maxSpeed = 3) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -7,7 +7,7 @@ class Car {
 
     this.speed = 0;
     this.acceleration = 0.2;
-    this.maxSpeed = 3;
+    this.maxSpeed = maxSpeed;
     this.friction = 0.05;
     this.angle = 0;
     this.damaged = false;
