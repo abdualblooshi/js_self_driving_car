f335df26caeea710a016a50585551d31597e7ae6
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-13 22:33:15 +0400

    Increased car speed to 4

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 3.25);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 4);
 const traffic = [new Car(road.getLaneCenter(2), -100, 30, 50, "AI", 2)];
 let carLocation = car.y;
 setTimeout(() => {
@@ -68,19 +68,48 @@ function passedCar(carLocation) {
   }
 }
 
+function generateRandomLocation(carLocation) {
+  let randomLocation = randomIntFromInterval(
+    carLocation - 1000,
+    carLocation + 1000
+  );
+  if (randomLocation === carLocation) {
+    randomLocation += 300;
+  }
+
+  return randomLocation;
+}
+
 function generateTraffic(carLocation) {
   let random = Math.random();
   if (random < 0.1) {
+    let randomLocation = generateRandomLocation(carLocation);
     let randomLane = Math.floor(Math.random() * 3);
     let randomCar = new Car(
       road.getLaneCenter(randomLane),
-      carLocation - 1000,
+      randomLocation,
       30,
       50,
       "AI",
       2
     );
     traffic.push(randomCar);
-    console.log(`Car generated at ${randomLane} lane and ${carLocation}`);
+    console.log(
+      `Front: Car generated at: ${randomLane} lane at: x=${randomLocation}`
+    );
+    let backRandomLocation = generateRandomLocation(carLocation);
+    let backRandomLane = Math.floor(Math.random() * 3);
+    let backRandomCar = new Car(
+      road.getLaneCenter(backRandomLane),
+      backRandomLocation,
+      30,
+      50,
+      "AI",
+      4
+    );
+    traffic.push(backRandomCar);
+    console.log(
+      `Back: Car generated at: ${backRandomCar} lane at: x=${backRandomCar}`
+    );
   }
 }
