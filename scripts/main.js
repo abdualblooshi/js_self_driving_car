3ac8fda34d2a5997986c5229e055a208e0e9bf17
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-13 22:14:41 +0400

    Changed initial traffic lane and user car speed

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,8 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 5);
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "AI", 2)];
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 3.25);
+const traffic = [new Car(road.getLaneCenter(2), -100, 30, 50, "AI", 2)];
 let carLocation = car.y;
 setTimeout(() => {
   setInterval(() => {
