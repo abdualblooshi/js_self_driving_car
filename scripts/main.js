9f66fa9800dba484abc6049b5f4473eb05c40016
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-10-13 18:42:53 +0400

    Added a maxSpeed of 2 for the AI cars

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ canvas.width = 200;
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "AI")];
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "AI", 2)];
 animate();
 
 function animate() {
